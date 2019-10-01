f6d2ca7b9220d09b5a3180d8d1555a5e65e65c17
Sebastian Wouters <sebastianwouters@gmail.com>
2019-10-01 13:36:53 +0200

    Compiler ...

--- compile.sh
+++ compile.sh
@@ -1,7 +1,6 @@
 #g++ -g\
 #icpc -flto -xHost -qopenmp -ipo -O3 -Wall\
-#g++ -O3 -march=native -flto -funroll-loops\
-g++ -g\
+g++ -O3 -march=native -flto -funroll-loops\
     src/main.cpp\
     src/zseb.cpp\
     src/huffman.cpp\
